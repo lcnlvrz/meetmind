23a115d63ca2893e93b93be0d0e27035236b9de6
Luciano Alvarez <lucianoalvarez1212@gmail.com>
2025-03-01 20:35:28 -0300

    feat(worker): implement audio chunk splitting by using chunk length and overlapping params

--- apps/worker/src/index.spec.ts
+++ apps/worker/src/index.spec.ts
@@ -1,33 +1,17 @@
 import * as path from 'path'
 
-console.log('dotenv path', path.resolve(__dirname, '../.env'))
-
 require('dotenv').config({
   path: path.resolve(__dirname, '../.env'),
 })
 
 import { test } from 'vitest'
-import { handler } from '.'
+import { bootstrapDependencies, processMeeting } from '.'
 
 test('should execute successfully', async () => {
-  await handler({
-    Records: [
-      {
-        body: JSON.stringify({
-          Records: [
-            {
-              s3: {
-                bucket: {
-                  name: 'meetmind-meetings',
-                },
-                object: {
-                  key: '2025-01-30_14-38-10.mkv',
-                },
-              },
-            },
-          ],
-        }),
-      },
-    ],
-  } as any)
+  const deps = bootstrapDependencies()
+
+  await processMeeting({
+    ...deps,
+    videoPath: path.join(__dirname, '../test/data/meeting-short.webm'),
+  })
 })
